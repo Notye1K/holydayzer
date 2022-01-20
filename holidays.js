e5d66a6c9ddfec92c694c3770040bb4973f57048
Kl3yton <kleyton_vccamargo@hotmail.com>
2022-01-20 16:29:44 -0300

    refactor: add some spaces

--- holidays.js
+++ holidays.js
@@ -1,14 +1,14 @@
 const holidays = [
-  { date: "01/01/2022", name: "Confraternização mundial" },
-  { date: "01/03/2022", name: "Carnaval" },
-  { date: "04/17/2022", name: "Páscoa" },
-  { date: "04/21/2022", name: "Tiradentes" },
-  { date: "05/01/2022", name: "Dia do trabalho" },
-  { date: "06/16/2022", name: "Corpus Christi" },
-  { date: "09/07/2022", name: "Independência do Brasil" },
-  { date: "10/12/2022", name: "Nossa Senhora Aparecida" },
-  { date: "11/02/2022", name: "Finados" },
-  { date: "11/15/2022", name: "Proclamação da República" },
-  { date: "12/25/2022", name: "Natal" }
+    { date: "01/01/2022", name: "Confraternização mundial" },
+    { date: "01/03/2022", name: "Carnaval" },
+    { date: "04/17/2022", name: "Páscoa" },
+    { date: "04/21/2022", name: "Tiradentes" },
+    { date: "05/01/2022", name: "Dia do trabalho" },
+    { date: "06/16/2022", name: "Corpus Christi" },
+    { date: "09/07/2022", name: "Independência do Brasil" },
+    { date: "10/12/2022", name: "Nossa Senhora Aparecida" },
+    { date: "11/02/2022", name: "Finados" },
+    { date: "11/15/2022", name: "Proclamação da República" },
+    { date: "12/25/2022", name: "Natal" }
 ]
 export default holidays
--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 import holidays from "./holidays.js"
-import  express  from "express"
+import express from "express"
 
 const server = express()
 
 server.get('/holidays', (req, res) => res.send(holidays))
+
 server.get('/is-today-holiday', (req, res) => {
     const hoje = new Date();
     const v = holidays.find(v => v.date === hoje.toLocaleDateString())
-    if (v === undefined){
+    if (v === undefined) {
         res.send('Não, hoje não é feriado')
     }
-    else{
+    else {
         res.send(`Sim, hoje é ${v.name}`)
     }
 })
+
 server.get('/holidays/:id', (req, res) => {
     let id = req.params.id
     const month = '0'
-    if (id.length === 1){
+    if (id.length === 1) {
         id = month + id
     }
     const v = holidays.filter(v => v.date.split('/')[0] === id)
-    if(v.length === 0){
+    if (v.length === 0) {
         res.send('Não tem feriado')
     }
-    else{
+    else {
         res.send(v)
     }
 })
